dc1bb07efa64a1dac20a1a067bfda1d6ce003b55
ronalyssa <ronalyssa99@gmail.com>
2021-04-16 12:09:17 -0500

    built out mvc files

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,2 +1,41 @@
 class WordsController < ApplicationController
+    def index
+        words = Word.all
+    end
+
+    def show
+        word = Word.find(params[:id])
+    end
+
+    def new
+        word = Word.new
+    end
+    
+    def create
+        word = Word.new(word_params)
+        if word.save 
+            redirect_to word_path(word)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        word = Word.find(params[:id])
+    end
+    
+    def update
+        word = Word.find(params[:id])
+        if word.update(word_params)
+            redirect_to word_path(word)
+        else
+            render :edit
+        end
+    end
+    private
+
+    def word_params
+        params.require(:word).permit(:word, :match,:player_id)
+        
+    end
 end
